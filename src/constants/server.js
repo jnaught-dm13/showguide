b40dc9d62ad3f58be2c304401616ac04f6988190
Aaron.Nacua <alnacua@gmail.com>
2018-05-04 19:15:17 -0500

    added  hosting endpoints

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,6 +1,6 @@
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "http://myapidomain.com"
+    ? "http://streamguide.aaronnacua.com"
     : "http://localhost:3001";
 
 export default PAYMENT_SERVER_URL;
